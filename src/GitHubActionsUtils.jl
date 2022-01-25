e4b07dbfed5dfdcc894b6dc32a087c381f96a061
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-01-25 12:03:59 +0100

    add a few functions

--- src/GitHubActionsUtils.jl
+++ src/GitHubActionsUtils.jl
@@ -1,5 +1,16 @@
 module GitHubActionsUtils
 
-# Write your package code here.
+function get_env(x)
+    get(ENV, x, nothing)
+end
+
+is_github_actions() = get_env("CI") == "true" && get_env("GITHUB_ACTIONS") == "true"
+event_name() = something(get_env("GITHUB_EVENT_NAME"))
+head_ref() = something(get_env("GITHUB_HEAD_REF"))
+github_ref() = something(get_env("GITHUB_REF"))
+is_branch() = something(get_env("GITHUB_REF_TYPE")) == "branch"
+branch_name() = is_branch() ? match(r"^refs/heads/(.*)", github_ref())[1] : nothing
+is_tag() = something(get_env("GITHUB_REF_TYPE")) == "tag"
+tag_name() = is_tag() ? match(r"^refs/tags/(.*)", github_ref())[1] : nothing
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using GitHubActionsUtils
 using Test
 
 @testset "GitHubActionsUtils.jl" begin
-    # Write your tests here.
+    @show GitHubActionsUtils.is_github_actions()
+    @show GitHubActionsUtils.event_name()
+    @show GitHubActionsUtils.head_ref()
+    @show GitHubActionsUtils.github_ref()
+    @show GitHubActionsUtils.is_branch()
+    @show GitHubActionsUtils.branch_name()
+    @show GitHubActionsUtils.is_tag()
+    @show GitHubActionsUtils.tag_name()
 end
